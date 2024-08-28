ebcc7ebb4ca0c0df066b1d87ec21183fa6a1664a
Gyokhan <gyokhan28@gmail.com>
2024-08-28 20:39:19 +0300

    Setup Java project Add exclusions for intellij and gradle files from .gitignore

---
+++ src/test/java/com/example/b2b_opportunities/B2bOpportunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b2b_opportunities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bOpportunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
