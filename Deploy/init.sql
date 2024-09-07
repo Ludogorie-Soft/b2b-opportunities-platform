a6a2a041e43db55d78b137117c353429e6e33975
aganchev <alexander.m.ganchev@gmail.com>
2024-09-08 00:29:32 +0300

    Add test postgres database

---
+++ Deploy/init.sql
@@ -0,0 +1,2 @@
+--CREATE DATABASE b2b_opportunities;
+CREATE DATABASE b2b_opportunities_test;
---
+++ src/test/java/com/example/b2b_opportunities/AuthControllerTest.java
@@ -0,0 +1,67 @@
+package com.example.b2b_opportunities;
+
+import com.example.b2b_opportunities.Dtos.Request.UserRequestDto;
+import com.example.b2b_opportunities.Service.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    private UserRequestDto userRequestDto;
+
+    @BeforeEach
+    public void setUp() {
+        userRequestDto = new UserRequestDto();
+        userRequestDto.setUsername("testuser");
+        userRequestDto.setFirstName("Test");
+        userRequestDto.setLastName("Test");
+        userRequestDto.setCompanyName("Test");
+        userRequestDto.setEmail("test@test.com");
+        userRequestDto.setPassword("password123");
+        userRequestDto.setRepeatedPassword("password123");
+    }
+
+    @Test
+    public void registerUserWithValidInput() throws Exception {
+
+        ResultActions resultActions = mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "testuser",
+                            "firstName": "Test",
+                            "lastName": "Test",
+                            "companyName": "Test",
+                            "email": "test@test.com",
+                            "password": "password123",
+                            "repeatedPassword": "password123"
+                        }
+                        """));
+
+        resultActions.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username", is("testuser")))
+                .andExpect(jsonPath("$.firstName", is("Test")))
+                .andExpect(jsonPath("$.lastName", is("Test")))
+                .andExpect(jsonPath("$.companyName", is("Test")))
+                .andExpect(jsonPath("$.email", is("test@test.com")));
+    }
+}
