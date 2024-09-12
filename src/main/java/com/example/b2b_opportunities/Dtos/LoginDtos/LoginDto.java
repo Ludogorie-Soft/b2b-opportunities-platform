b28cc5f1ff1d41c52dc7b6ae8dc46a6426157b3a
Yordanoff <ivailo.yordanov.ph@gmail.com>
2024-09-12 13:35:55 +0300

    use testcontainers for testing

--- src/main/java/com/example/b2b_opportunities/Dtos/LoginDtos/LoginDto.java
+++ src/main/java/com/example/b2b_opportunities/Dtos/LoginDtos/LoginDto.java
@@ -1,10 +1,12 @@
 package com.example.b2b_opportunities.Dtos.LoginDtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LoginDto {
     private String usernameOrEmail;
     private String password;
--- src/test/java/com/example/b2b_opportunities/AuthControllerTest.java
+++ src/test/java/com/example/b2b_opportunities/AuthControllerTest.java
@@ -1,67 +1,172 @@
 package com.example.b2b_opportunities;
 
+import com.example.b2b_opportunities.Dtos.LoginDtos.LoginDto;
 import com.example.b2b_opportunities.Dtos.Request.UserRequestDto;
+import com.example.b2b_opportunities.Dtos.Response.UserResponseDto;
 import com.example.b2b_opportunities.Service.AuthenticationService;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.*;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AuthControllerTest {
+@Transactional  // Roll back changes made by tests, ensuring each test starts with a clean state.
+public class AuthControllerTest extends BaseTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private AuthenticationService authenticationService;
 
-    private UserRequestDto userRequestDto;
+    private final UserRequestDto userRequestDto = new UserRequestDto(
+            "Test-User",
+            "Test",
+            "User",
+            "TestCompany",
+            "TestUSER@example.Com",
+            "password123",
+            "password123"
+    );
 
-    @BeforeEach
-    public void setUp() {
-        userRequestDto = new UserRequestDto();
-        userRequestDto.setUsername("testuser");
-        userRequestDto.setFirstName("Test");
-        userRequestDto.setLastName("Test");
-        userRequestDto.setCompanyName("Test");
-        userRequestDto.setEmail("test@test.com");
-        userRequestDto.setPassword("password123");
-        userRequestDto.setRepeatedPassword("password123");
+    @Test
+    void shouldRegisterUserAndSaveToDatabase() throws Exception {
+        // Convert the DTO to JSON
+        String userRequestJson = objectMapper.writeValueAsString(userRequestDto);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username", is("test-user")))
+                .andExpect(jsonPath("$.firstName", is("Test")))
+                .andExpect(jsonPath("$.lastName", is("User")))
+                .andExpect(jsonPath("$.email", is("testuser@example.com")));
+
+        assertTrue(authenticationService.isUsernameInDB("test-user"));
     }
 
     @Test
-    public void registerUserWithValidInput() throws Exception {
+    void shouldNotRegisterSameEmailTwice() throws Exception {
+        // Convert the DTO to JSON
+        String userRequestJson = objectMapper.writeValueAsString(userRequestDto);
 
-        ResultActions resultActions = mockMvc.perform(post("/api/auth/register")
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isCreated());
+
+        UserRequestDto userRequestDto2 = userRequestDto;
+        userRequestDto2.setUsername("new-user-name");
+        String userRequestJson2 = objectMapper.writeValueAsString(userRequestDto2);
+
+        // Test with same email
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson2))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message", is("Email already in use. Please use a different email")));
+
+        assertTrue(authenticationService.isUsernameInDB("test-user"));
+
+        List<UserResponseDto> users = authenticationService.getAllUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void shouldNotRegisterSameUsernameTwice() throws Exception {
+        // Convert the DTO to JSON
+        String userRequestJson = objectMapper.writeValueAsString(userRequestDto);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isCreated());
+
+        UserRequestDto userRequestDto2 = userRequestDto;
+        userRequestDto2.setEmail("new-free-email@abv.bg");
+        String userRequestJson2 = objectMapper.writeValueAsString(userRequestDto2);
+
+        // Test with same username
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson2))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message", is("Username already in use. Please use a different username")));
+
+        assertTrue(authenticationService.isUsernameInDB("test-user"));
+
+        List<UserResponseDto> users = authenticationService.getAllUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void shouldNotRegisterUserWithWrongRepeatPassword() throws Exception {
+        // Convert the DTO to JSON
+        String userRequestJson = objectMapper.writeValueAsString(userRequestDto);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isCreated());
+
+        UserRequestDto userRequestDto2 = userRequestDto;
+        userRequestDto2.setEmail("new-free-email@abv.bg");
+        userRequestDto2.setUsername("new-free-username");
+        userRequestDto2.setRepeatedPassword("wrong-password");
+        String userRequestJson2 = objectMapper.writeValueAsString(userRequestDto2);
+
+        // Test with wrong second password
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson2))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message", is("Passwords don't match")));
+
+        assertTrue(authenticationService.isUsernameInDB("test-user"));
+        assertFalse(authenticationService.isUsernameInDB("new-free-username"));
+
+        List<UserResponseDto> users = authenticationService.getAllUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void shouldLoginAndReturnToken() throws Exception {
+
+        String userRequestJson = objectMapper.writeValueAsString(userRequestDto);
+
+        mockMvc.perform(post("/api/auth/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                        {
-                            "username": "testuser",
-                            "firstName": "Test",
-                            "lastName": "Test",
-                            "companyName": "Test",
-                            "email": "test@test.com",
-                            "password": "password123",
-                            "repeatedPassword": "password123"
-                        }
-                        """));
+                .content(userRequestJson));
 
-        resultActions.andExpect(status().isCreated())
-                .andExpect(jsonPath("$.username", is("testuser")))
-                .andExpect(jsonPath("$.firstName", is("Test")))
-                .andExpect(jsonPath("$.lastName", is("Test")))
-                .andExpect(jsonPath("$.companyName", is("Test")))
-                .andExpect(jsonPath("$.email", is("test@test.com")));
+        LoginDto loginDto = new LoginDto(userRequestDto.getEmail(), userRequestDto.getPassword());
+
+        String loginDtoJson = objectMapper.writeValueAsString(loginDto);
+
+
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginDtoJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.expiresIn", is(3600000)));
     }
 }
