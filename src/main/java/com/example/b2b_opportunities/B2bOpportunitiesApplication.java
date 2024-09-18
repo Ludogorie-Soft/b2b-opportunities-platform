1d46f458cf3a35c3d0b204454d3e5a2446299654
Ivo <ivailo.yordanov.ph@gmail.com>
2024-09-18 16:25:02 +0300

    update README.md

--- src/main/java/com/example/b2b_opportunities/B2bOpportunitiesApplication.java
+++ src/main/java/com/example/b2b_opportunities/B2bOpportunitiesApplication.java
@@ -1,9 +1,17 @@
 package com.example.b2b_opportunities;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "B2B",
+                version = "1.0",
+                description = "Backend API"
+        ))
 public class B2bOpportunitiesApplication {
 
     public static void main(String[] args) {
