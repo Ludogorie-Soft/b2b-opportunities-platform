c6e52b13fd1aa46d6489aa27d2cfa0d35494612b
Ivo <ivailo.yordanov.ph@gmail.com>
2024-10-01 14:38:32 +0300

    add additional checks for domain:trim, blanks, id instead name

--- src/main/java/com/example/b2b_opportunities/Controller/DomainController.java
+++ src/main/java/com/example/b2b_opportunities/Controller/DomainController.java
@@ -2,6 +2,7 @@ package com.example.b2b_opportunities.Controller;
 
 import com.example.b2b_opportunities.Entity.Domain;
 import com.example.b2b_opportunities.Exception.AlreadyExistsException;
+import com.example.b2b_opportunities.Exception.InvalidInputException;
 import com.example.b2b_opportunities.Exception.NotFoundException;
 import com.example.b2b_opportunities.Repository.DomainRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/domains")
@@ -25,36 +27,54 @@ public class DomainController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Domain> getDomains() {
+    public List<Domain> getAll() {
         return domainRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Domain addDomain(@RequestParam String name) {
-        checkIfAlreadyExists(name);
+    public Domain add(@RequestParam String name) {
+        name = name.trim();
+        validateNameNotBlank(name);
+        validateNameDoesNotExists(name);
         return domainRepository.save(Domain.builder().name(name).build());
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
-    public Domain editDomain(@RequestParam String name, @RequestParam String newName) {
-        Domain domain = domainRepository.findByName(name).orElseThrow(() -> new NotFoundException("Domain with name " + name + " not found"));
-        checkIfAlreadyExists(newName);
+    public Domain edit(@RequestParam Long id, @RequestParam String newName) {
+        Domain domain = findDomainByIdOrThrow(id);
+
+        newName = newName.trim();
+        validateNameNotBlank(newName);
+        if (Objects.equals(domain.getName(), newName)) {
+            return domain;
+        }
+        validateNameDoesNotExists(newName);
+
         domain.setName(newName);
         return domainRepository.save(domain);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteDomain(@RequestParam("id") Long id) {
-        Domain domain = domainRepository.findById(id).orElseThrow(() -> new NotFoundException("Domain not found"));
-        domainRepository.delete(domain);
+    public void delete(@RequestParam Long id) {
+        domainRepository.delete(findDomainByIdOrThrow(id));
     }
 
-    private void checkIfAlreadyExists(String name) {
+    private void validateNameDoesNotExists(String name) {
         if (domainRepository.findByName(name).isPresent()) {
-            throw new AlreadyExistsException("Domain already exists");
+            throw new AlreadyExistsException("Domain with name: '" + name + "' already exists");
+        }
+    }
+
+    private Domain findDomainByIdOrThrow(Long id) {
+        return domainRepository.findById(id).orElseThrow(() -> new NotFoundException("Domain with ID: " + id + " not found"));
+    }
+
+    private void validateNameNotBlank(String name) {
+        if (name.isBlank()) {
+            throw new InvalidInputException("Domain name cannot be blank");
         }
     }
 }
