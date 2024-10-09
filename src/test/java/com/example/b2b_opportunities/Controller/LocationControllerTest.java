d742fb516cf0b188685c4b81b3261c79dd09a637
Ivo <ivailo.yordanov.ph@gmail.com>
2024-10-09 13:04:19 +0300

    fix issue with 2x @Transactional for BaseTest

--- src/test/java/com/example/b2b_opportunities/Controller/LocationControllerTest.java
+++ src/test/java/com/example/b2b_opportunities/Controller/LocationControllerTest.java
@@ -8,7 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.nio.file.Paths;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -22,7 +31,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc(addFilters = false)
-class LocationControllerTest extends BaseTest {
+@Testcontainers
+class LocationControllerTest {
+    private final static String hostPath = Paths.get("Deploy/icons").toAbsolutePath().toString();
+    private final static String containerPath = "/icons";
+
+    @Container
+    protected static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
+            .withExposedPorts(5432) // waits for the port to be available
+            .withFileSystemBind(hostPath, containerPath, BindMode.READ_ONLY);
+
+
+    @DynamicPropertySource
+    public static void overridePropertiesFile(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Autowired
     private LocationRepository locationRepository;
 
