846ebc1cf32a795c693352fd20a61d82f03e78b3
Ivo <ivailo.yordanov.ph@gmail.com>
2024-10-14 13:27:43 +0300

    extend Base test, use Builder, move private to bottom

--- src/test/java/com/example/b2b_opportunities/Controller/PatternControllerTest.java
+++ src/test/java/com/example/b2b_opportunities/Controller/PatternControllerTest.java
@@ -1,5 +1,6 @@
-package com.example.b2b_opportunities.Service;
+package com.example.b2b_opportunities.Controller;
 
+import com.example.b2b_opportunities.BaseTest;
 import com.example.b2b_opportunities.Dto.Request.PatternRequestDto;
 import com.example.b2b_opportunities.Entity.Pattern;
 import com.example.b2b_opportunities.Entity.Skill;
@@ -12,44 +13,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
-public class PatternControllerTest {
-
-    private static final String HOST_PATH = Paths.get("Deploy/icons").toAbsolutePath().toString();
-    private static final String CONTAINER_PATH = "/icons";
-
-    @Container
-    protected static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
-            .withExposedPorts(5432)
-            .withFileSystemBind(HOST_PATH, CONTAINER_PATH, BindMode.READ_ONLY);
-
-    @DynamicPropertySource
-    public static void overridePropertiesFile(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
-
+public class PatternControllerTest extends BaseTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -66,53 +46,15 @@ public class PatternControllerTest {
         patternRepository.deleteAll();
         skillRepository.deleteAll();
 
-        Skill firstSkill = new Skill();
-        firstSkill.setName("first Skill");
-        firstSkill.setAssignable(true);
-        skillRepository.save(firstSkill);
+        Skill s1 = skillRepository.save(Skill.builder().name("first Skill").assignable(true).build());
+        Skill s2 = skillRepository.save(Skill.builder().name("second Skill").assignable(true).build());
 
-        Skill secondSkill = new Skill();
-        secondSkill.setName("second Skill");
-        secondSkill.setAssignable(true);
-        skillRepository.save(secondSkill);
-
-        List<Skill> skillsSet = new ArrayList<>();
-
-        skillsSet.add(firstSkill);
-        skillsSet.add(secondSkill);
+        List<Skill> skills = List.of(s1, s2);
 
         pattern = new Pattern();
         pattern.setName("Test Pattern");
-        pattern.setSuggestedSkills(skillsSet);
+        pattern.setSuggestedSkills(skills);
         pattern = patternRepository.save(pattern);
-    }
-
-    private String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private ResultActions performGetResult(String url) throws Exception {
-        return mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON));
-    }
-
-    private ResultActions performPostResult(PatternRequestDto patternRequestDto) throws Exception {
-        return mockMvc.perform(post("/patterns")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(patternRequestDto)));
-    }
-
-    private ResultActions performPutResult(PatternRequestDto patternRequestDto) throws Exception {
-        return mockMvc.perform(put("/patterns")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(patternRequestDto)));
-    }
-
-    private ResultActions performDeleteResult(String url) throws Exception {
-        return mockMvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -181,5 +123,33 @@ public class PatternControllerTest {
     void deletePatternNotFoundTest() throws Exception {
         performDeleteResult("/patterns?id=359")
                 .andExpect(status().isNotFound());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ResultActions performGetResult(String url) throws Exception {
+        return mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON));
+    }
+
+    private ResultActions performPostResult(PatternRequestDto patternRequestDto) throws Exception {
+        return mockMvc.perform(post("/patterns")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(patternRequestDto)));
+    }
+
+    private ResultActions performPutResult(PatternRequestDto patternRequestDto) throws Exception {
+        return mockMvc.perform(put("/patterns")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(patternRequestDto)));
+    }
+
+    private ResultActions performDeleteResult(String url) throws Exception {
+        return mockMvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON));
     }
 }
