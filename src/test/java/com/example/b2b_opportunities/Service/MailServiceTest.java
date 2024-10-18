c9f73903ee54cec82052c5a447c82dc29044220d
Gyokhan <gyokhan28@gmail.com>
2024-10-18 16:12:49 +0300

    Adapt a failing test

--- src/test/java/com/example/b2b_opportunities/Service/MailServiceTest.java
+++ src/test/java/com/example/b2b_opportunities/Service/MailServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.b2b_opportunities.Service;
 
+import com.example.b2b_opportunities.Dto.Request.EmailRequest;
 import com.example.b2b_opportunities.Entity.User;
 import com.example.b2b_opportunities.Repository.ConfirmationTokenRepository;
 import jakarta.mail.MessagingException;
@@ -11,9 +12,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,6 +29,8 @@ class MailServiceTest {
     @Mock
     private JavaMailSender mailSender;
 
+    @Mock
+    private RestTemplate restTemplate;
     @InjectMocks
     private MailService mailService;
 
@@ -37,21 +45,21 @@ class MailServiceTest {
 
     @BeforeEach
     void setUp() {
-        ReflectionTestUtils.setField(mailService, "fromMail", "sender@test.com");
+        ReflectionTestUtils.setField(mailService, "emailServiceUrl", "http://mocked-email-service.com/send");
+
     }
 
     @Test
-    void testSendConfirmationMail() throws MessagingException {
+    void testSendConfirmationMail() {
         User user = new User();
         user.setFirstName("Test");
         user.setEmail("test@test.com");
 
-        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+        when(restTemplate.postForObject(any(String.class), any(EmailRequest.class), eq(String.class)))
+                .thenReturn("Email sent successfully");
 
         mailService.sendConfirmationMail(user, request);
 
-        verify(mailSender, times(1)).send(mimeMessage);
-        verify(mailSender, times(1)).createMimeMessage();
-        verify(mimeMessage, times(1)).setSubject("Confirm your E-Mail - B2B Opportunities");
+        verify(restTemplate, times(1)).postForObject(any(String.class), any(EmailRequest.class), eq(String.class));
     }
 }
