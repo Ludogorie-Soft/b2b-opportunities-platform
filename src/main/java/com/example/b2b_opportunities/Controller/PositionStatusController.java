0823ba7c3754ea9d069285bcaf0bd2481f07d698
Ivo <ivailo.yordanov.ph@gmail.com>
2024-10-28 11:14:45 +0200

    Add Integration tests for PositionStatus Controller

--- src/main/java/com/example/b2b_opportunities/Controller/PositionStatusController.java
+++ src/main/java/com/example/b2b_opportunities/Controller/PositionStatusController.java
@@ -4,9 +4,11 @@ import com.example.b2b_opportunities.Entity.PositionStatus;
 import com.example.b2b_opportunities.Exception.common.NotFoundException;
 import com.example.b2b_opportunities.Repository.PositionStatusRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,11 +20,13 @@ public class PositionStatusController {
     private final PositionStatusRepository positionStatusRepository;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<PositionStatus> get() {
         return positionStatusRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public PositionStatus getById(@PathVariable("id") Long id) {
         return positionStatusRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Position status with ID: " + id + " not found"));
