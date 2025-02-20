f11040e80a56b1a6e36df163b81a32cc8e502c04
DASKALA <prdimitrov97@gmail.com>
2025-02-20 12:23:14 +0200

    Make endpoint for returning partial positions list - Create CompanyPositionsResponseDto class - Add controller method, that returns partial positions list - Create custom query method in PositionRepository interface - Add method in PositionService interface - Create getCompanyPositions method in PositionServiceImpl class.

--- src/main/java/com/example/b2b_opportunities/Service/Interface/PositionService.java
+++ src/main/java/com/example/b2b_opportunities/Service/Interface/PositionService.java
@@ -2,10 +2,12 @@ package com.example.b2b_opportunities.Service.Interface;
 
 import com.example.b2b_opportunities.Dto.Request.PositionEditRequestDto;
 import com.example.b2b_opportunities.Dto.Request.PositionRequestDto;
+import com.example.b2b_opportunities.Dto.Response.CompanyPositionsResponseDto;
 import com.example.b2b_opportunities.Dto.Response.PositionResponseDto;
 import com.example.b2b_opportunities.Entity.Position;
 import org.springframework.security.core.Authentication;
 
+import java.util.List;
 import java.util.Set;
 
 public interface PositionService {
@@ -22,4 +24,6 @@ public interface PositionService {
     void editPositionStatus(Long positionId, Long statusId, String customCloseReason, Authentication authentication);
 
     Position getPositionOrThrow(Long id);
+
+    List<CompanyPositionsResponseDto> getCompanyPositions(Authentication authentication, Long id);
 }
