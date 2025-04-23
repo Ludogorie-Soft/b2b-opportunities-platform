bc54257476635f5b6392153bae70eadc47bfbbcf
Gyokhan <gyokhan28@gmail.com>
2025-04-23 09:43:31 +0300

    reorder migration

--- src/main/resources/db/changelog/49-clear-test-data.sql
+++ src/main/resources/db/changelog/49-clear-test-data.sql
@@ -1,30 +1,40 @@
 DELETE FROM confirmation_token;
 
-DELETE FROM users
-WHERE username != 'admin';
+DELETE FROM position_applications;
 
-DELETE FROM talents;
-DELETE FROM talent_work_modes;
-DELETE FROM skill_experience;
-DELETE FROM talent_experience;
-DELETE FROM talent_locations;
-DELETE FROM required_skill;
+DELETE FROM filter_skills;
+
+DELETE FROM company_talent_access_groups;
+
+DELETE FROM company_skills;
+
+DELETE FROM company_project_notified;
+
+DELETE FROM company_partner_groups;
+
 DELETE FROM positions_optional_skills;
+
 DELETE FROM position_responsibilities;
+
 DELETE FROM position_work_modes;
-DELETE FROM positions;
-DELETE FROM rates;
-DELETE FROM projects;
+
+DELETE FROM required_skill;
+
+DELETE FROM skill_experience;
+
+DELETE FROM talent_work_modes;
+
+DELETE FROM talent_locations;
+
 DELETE FROM partner_group_companies;
 DELETE FROM project_partner_group;
+DELETE FROM positions;
+DELETE FROM projects;
 DELETE FROM partner_groups;
-DELETE FROM position_applications;
-DELETE FROM filter_skills;
 DELETE FROM filters;
-DELETE FROM company_talent_access_groups;
-DELETE FROM company_skills;
-DELETE FROM company_project_notified;
-DELETE FROM company_partner_groups;
 
 DELETE FROM companies
 WHERE name != 'Test Company';
+
+DELETE FROM users
+WHERE username != 'admin';
