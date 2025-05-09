5b7e8c1a21114d957f54f5986c36a82ca0aee00d
Gyokhan <gyokhan28@gmail.com>
2025-05-09 12:20:22 +0300

    Add AI Skills

---
+++ src/main/resources/db/changelog/50-add-ai-skills.sql
@@ -0,0 +1,12 @@
+INSERT INTO skills (id, name, parent_id, assignable, image, image_type) VALUES
+(148, 'AI', 2, false, NULL, NULL),
+(149, 'AI Model Training', 148, true, NULL, NULL),
+(150, 'AI Agents', 148, true, NULL, NULL),
+(151, 'Embeddings API', 148, true, NULL, NULL),
+(152, 'RAG (Retrieval-Augmented Generation)', 148, true, NULL, NULL),
+(153, 'Multimodal AI', 148, true, NULL, NULL),
+(154, 'Vector Databases', 148, true, NULL, NULL),
+(155, 'Large Language Models (LLMs)', 148, true, NULL, NULL),
+(156, 'Computer Vision', 148, true, NULL, NULL),
+(157, 'Natural Language Processing (NLP)', 148, true, NULL, NULL),
+(158, 'AI Ops', 148, true, NULL, NULL);
